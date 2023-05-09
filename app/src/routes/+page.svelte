ab4aa3cb65786e6f2442cdacd160dc45fee8d53c
Paquette1111 <MatthewPaquette@live.com>
2023-05-09 01:07:26 -0400

    init

---
+++ app/src/routes/+page.svelte
@@ -0,0 +1,50 @@
+<script>
+
+</script>
+
+<div class="w-full flex flex-col items-center mt-[150px]">
+    <h1 class="logo text-8xl md:text-9xl text-white">Shrook</h1>
+    <h2 class="text-white font-semibold mt-3">Cronos #1 token (don't google it.)</h2>
+</div>
+
+<img src="shrook.png" alt="Shrook is life. Shrook is love.">
+
+<div class="flex flex-row gap-3 flex-wrap justify-center items-center mt-5">
+
+    <a href="https://twitter.com/ShrookCro" class="relative inline-flex items-center justify-center inline-block p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 rounded-lg shadow-2xl group">
+        <span class="absolute top-0 left-0 w-40 h-40 -mt-10 -ml-3 transition-all duration-700 bg-red-500 rounded-full blur-md ease"></span>
+        <span class="absolute inset-0 w-full h-full transition duration-700 group-hover:rotate-180 ease">
+            <span class="absolute bottom-0 left-0 w-24 h-24 -ml-10 bg-purple-500 rounded-full blur-md"></span>
+            <span class="absolute bottom-0 right-0 w-24 h-24 -mr-10 bg-pink-500 rounded-full blur-md"></span>
+        </span>
+        <span class="relative text-white">Twitter</span>
+    </a>
+
+    <a href="https://dexscreener.com/cronos/0x4a27d7ab3110acc6eae84705e4b5c923e0007e37" class="relative inline-flex items-center justify-center inline-block p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 rounded-lg shadow-2xl group">
+        <span class="absolute top-0 left-0 w-40 h-40 -mt-10 -ml-3 transition-all duration-700 bg-red-500 rounded-full blur-md ease"></span>
+        <span class="absolute inset-0 w-full h-full transition duration-700 group-hover:rotate-180 ease">
+            <span class="absolute bottom-0 left-0 w-24 h-24 -ml-10 bg-purple-500 rounded-full blur-md"></span>
+            <span class="absolute bottom-0 right-0 w-24 h-24 -mr-10 bg-pink-500 rounded-full blur-md"></span>
+        </span>
+        <span class="relative text-white">Charts</span>
+    </a>
+
+    <a href="https://swap.crodex.app/#/swap?outputCurrency=0x8ab05cb10b3c15c54477961d18bda89ac8d777ef" class="relative inline-flex items-center justify-center inline-block p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 rounded-lg shadow-2xl group">
+        <span class="absolute top-0 left-0 w-40 h-40 -mt-10 -ml-3 transition-all duration-700 bg-red-500 rounded-full blur-md ease"></span>
+        <span class="absolute inset-0 w-full h-full transition duration-700 group-hover:rotate-180 ease">
+            <span class="absolute bottom-0 left-0 w-24 h-24 -ml-10 bg-purple-500 rounded-full blur-md"></span>
+            <span class="absolute bottom-0 right-0 w-24 h-24 -mr-10 bg-pink-500 rounded-full blur-md"></span>
+        </span>
+        <span class="relative text-white">Swap</span>
+    </a>
+
+    <a href="#" class="relative inline-flex items-center justify-center inline-block p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 rounded-lg shadow-2xl group">
+        <span class="absolute top-0 left-0 w-40 h-40 -mt-10 -ml-3 transition-all duration-700 bg-red-500 rounded-full blur-md ease"></span>
+        <span class="absolute inset-0 w-full h-full transition duration-700 group-hover:rotate-180 ease">
+            <span class="absolute bottom-0 left-0 w-24 h-24 -ml-10 bg-purple-500 rounded-full blur-md"></span>
+            <span class="absolute bottom-0 right-0 w-24 h-24 -mr-10 bg-pink-500 rounded-full blur-md"></span>
+        </span>
+        <span class="relative text-white">Telegram (Soon)</span>
+    </a>
+
+</div>
